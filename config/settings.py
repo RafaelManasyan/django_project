e0b080374ce0f2a49348bbacf1a3d2ee05e3eb86
rafaelmanasyan <workpageraf@gmail.com>
2024-11-08 17:14:49 +0300

    "configure settings.py and add .env.sample"

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,17 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-n6v1_vl(!p(pz)rz&e_pz!7*nda94$241_^f_szcj*1hxwjy6-'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -58,8 +63,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
