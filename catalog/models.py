85f142c3d66c7a7f8f475b7bb067311dcef5b4b3
rafaelmanasyan <workpageraf@gmail.com>
2024-11-08 19:33:19 +0300

    "add Product and Category modules"

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(verbose_name='Изображение')
+    category = models.CharField(max_length=150, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    created_at = models.DateField(verbose_name='Дата создания продукта')
+    updated_at = models.DateField(verbose_name='Дата последних изменений')
+
+    def __str__(self):
+        return f'''Наименование - {self.name}. Цена - {self.price}.'''
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'Категория - {self.name}. Описание: {self.description}.'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
