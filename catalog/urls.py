17d67992f2d0f48dc227ef0cebb5b34004598a8f
rafaelmanasyan <workpageraf@gmail.com>
2024-11-16 17:36:29 +0300

    "add templates"

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,8 @@ app_name = 'catalog'
 
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts')
+    path('main/', views.main, name='main'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('products_list/', views.products_list, name='products_list'),
+    path('product/<int:pk>/', views.product, name='product')
 ]
